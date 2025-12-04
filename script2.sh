4e26ef82e422cc9e0b10ffcc61dc90ace2e44188
20isaak23 <sakabenalcazar@gmail.com>
2025-12-04 13:22:27 +0000

    Script2

---
+++ script2.sh
@@ -0,0 +1,14 @@
+uname -a
+# Viene de Unix Name. Si lo ejecutas solo, 
+#solo muestra el tipo de sistema operativo
+# (por lo general, "Linux").
+#-a: Viene de all (todo). Le pide al comando que muestre toda 
+#la información disponible del sistema.
+lscpu
+#Este comando se utiliza para mostrar la información de la CPU (Unidad Central de Procesamiento o procesador).
+free -h
+#Este comando se utiliza para mostrar la información 
+#del uso de la memoria RAM (Memoria de Acceso Aleatorio) 
+#y la memoria swap.
+df -h
+#Es el medidor de disco duro de tu sistema.
